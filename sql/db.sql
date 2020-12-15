455dcc90ab42e7864e1ddab356000ded9e068798
Nilo <nlajunior@gmail.com>
2020-12-15 17:24:44 -0300

    Criaçao da tabela monitor

--- sql/db.sql
+++ sql/db.sql
@@ -1,45 +1,54 @@
-create database db;
+create database db3;
 
-use db;
+use db3;
 
 CREATE TABLE role (
-  id integer AUTO_INCREMENT,
-  name varchar(40) NOT NULL,
+  id int AUTO_INCREMENT,
+  name varchar(40) unique NOT NULL,
   actived tinyint(1) DEFAULT 1,
-  PRIMARY KEY (id),
-  UNIQUE KEY name (name)
+  PRIMARY KEY (id)
+ 
 );
 
 CREATE TABLE user (
   id int NOT NULL AUTO_INCREMENT,
-  login varchar(40) NOT NULL,
+  login varchar(40) unique NOT NULL,
   password varchar(200) NOT NULL,
-  organization_key varchar(200) NOT NULL,
-  email varchar(100) DEFAULT NULL,
-  role int DEFAULT NULL,
+  organization_key varchar(200) unique NOT NULL,
+  email varchar(100),
+  role int NOT NULL,
   actived tinyint(1) DEFAULT 0,
-  PRIMARY KEY (id),
-  UNIQUE KEY login (login),
-  UNIQUE KEY organization_key (organization_key),
+  PRIMARY KEY (id), 
   CONSTRAINT user_fk_1 FOREIGN KEY (role) REFERENCES role (id)
 );
 
 CREATE TABLE device (
   id int NOT NULL AUTO_INCREMENT,
-  mac varchar(100) NOT NULL,
+  mac varchar(30) unique NOT NULL,
   date_created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   status varchar(3) NOT NULL DEFAULT 'OFF',
   PRIMARY KEY (id)
+);
+
+CREATE TABLE monitor(
+   id int NOT NULL AUTO_INCREMENT,
+   monitor_id varchar(60) not null unique,
+   date_created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   status varchar(50) NOT NULL,
+   device_id varchar(30),
+   CONSTRAINT monitor_fk_1 FOREIGN KEY (device_id) REFERENCES device (mac),
+   PRIMARY KEY (id)
 );
 
 CREATE TABLE test (
   id int NOT NULL AUTO_INCREMENT,
   duration int NOT NULL,
   fhr_value float NOT NULL,
-  session_id varchar(100) NOT NULL,
   date_created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  device_id int DEFAULT NULL,
-  CONSTRAINT device_fk_1 FOREIGN KEY (device_id) REFERENCES role (id),
+  monitor_id varchar(100) NOT NULL,
+  device_id varchar(30) NOT NULL,
+  CONSTRAINT test_fk_1 FOREIGN KEY (device_id) REFERENCES device (mac),
+  CONSTRAINT test_fk_2 FOREIGN KEY (monitor_id) REFERENCES monitor (monitor_id),
   PRIMARY KEY (id)
 );
 
@@ -47,14 +56,16 @@ INSERT INTO `role`(id, name) VALUES (1,'Administrator'),(2,'Customer');
 
 INSERT INTO `user`(login, email, password, organization_key, role) VALUES ('admin','adminapi@sfiecorg.br','N0xdeBrCCKDqQXPALLcAm2}iu^GkBe4','ysb_92=qe#dgjf8%0ng+a*#4rt#5%3*4kw5%i2bck*gn@w3@f&-&', 1),('userapi','userapi@yourapi.org.br', 'N0xdeBrCCKDqQXPALLcAm2}iu^GkBe4','ysb_92=qe#dgjf8%0ng+a*#4rt#5%3*4kw5%i2bck*gn@w3@f&-&a',2);
 
-INSERT INTO device (mac, status) VALUES ('80:7D:3A:DA:D9:F4','ON'),
-('80:7D:3A:DA:D9:F5','ON'),
-('80:7D:3A:DA:D9:F6','ON'),
-('84:0D:8E:17:4D:C8','ON'),
-('8C:AA:B5:85:EE:14','ON')
+INSERT INTO device (mac) VALUES ('80:7D:3A:DA:D9:F4'),
+('80:7D:3A:DA:D9:F5'),
+('80:7D:3A:DA:D9:F6'),
+('84:0D:8E:17:4D:C8'),
+('8C:AA:B5:85:EE:14')
 ;
 
-create user 'userapi'@'localhost' identified by 'S3n@1IstH@pV1d@';
-grant all on db.* to 'userapi'@'localhost'
+#update user set password='N0xdeBrCCKDqQXPALLcAm2}iu^GkBe4' where id=1
 
+select * from role;
+select * from user;
+select * from device
 
